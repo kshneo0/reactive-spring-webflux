6ce0b15f3b7dd4b57778c6dcf81faa8d0f740eb0
kshneo0 <kshneo@gmail.com>
2022-04-13 05:57:37 +0900

    16. Testing Flux using JUnit5

--- reactive-programming-using-reactor/src/main/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorService.java
+++ reactive-programming-using-reactor/src/main/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorService.java
@@ -9,13 +9,14 @@ public class FluxAndMonoGeneratorService {
 	
     public Flux<String> namesFlux() {
         
-        return Flux.fromIterable(List.of("alex", "ben", "chloe")); // coming from a db or remote service
+        return Flux.fromIterable(List.of("alex", "ben", "chloe"))
+        		.log(); // coming from a db or remote service
 
     }
     
     public Mono<String> namesMono() {
 
-        return Mono.just("alex");
+        return Mono.just("alex").log();
 
     }
     
@@ -25,6 +26,8 @@ public class FluxAndMonoGeneratorService {
 		
 		fluxAndMonoGeneratorService.namesFlux()
 			.subscribe(name -> System.out.println("Flux Name is : " + name));
+		
+		System.out.println("-------------------------------");
 		
 		fluxAndMonoGeneratorService.namesMono()
 			.subscribe((name) -> System.out.println("Mono Name is : " + name));
