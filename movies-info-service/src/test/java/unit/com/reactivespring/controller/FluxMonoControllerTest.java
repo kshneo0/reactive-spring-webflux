d84388c7f27458a61299f74efc37b35bcbc633a2
kshneo0 <kshneo@gmail.com>
2022-04-14 06:13:56 +0900

    43. JUnit Test Streaming Endpoint - SSE

--- movies-info-service/src/test/java/unit/com/reactivespring/controller/FluxMonoControllerTest.java
+++ movies-info-service/src/test/java/unit/com/reactivespring/controller/FluxMonoControllerTest.java
@@ -49,6 +49,23 @@ class FluxMonoControllerTest {
 	}
 	
 	@Test
+	void stream() {
+		var flux = webTestClient
+					.get()
+					.uri("/stream")
+					.exchange()
+					.expectStatus()
+					.is2xxSuccessful()
+					.returnResult(Long.class)
+					.getResponseBody();
+		
+		StepVerifier.create(flux)
+			.expectNext(0l,1L,2L,3L)
+			.thenCancel()
+			.verify();
+	}
+	
+	@Test
 	void flux_approach3() {
 		webTestClient
 				.get()
