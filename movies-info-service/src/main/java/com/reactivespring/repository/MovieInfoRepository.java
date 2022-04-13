38001728ebd87c91e2918bb183dcd063186b7901
kshneo0 <kshneo@gmail.com>
2022-04-14 07:39:04 +0900

    45. Configure the ReactiveMongoDB Repository for MovieInfo Document

---
+++ movies-info-service/src/main/java/com/reactivespring/repository/MovieInfoRepository.java
@@ -0,0 +1,9 @@
+package com.reactivespring.repository;
+
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+
+import com.reactivespring.domain.MovieInfo;
+
+public interface MovieInfoRepository extends ReactiveMongoRepository<MovieInfo, String>{
+
+}
