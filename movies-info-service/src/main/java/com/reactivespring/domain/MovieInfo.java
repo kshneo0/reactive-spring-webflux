8cddd516f55640e1e3422ea72f8c29f8d9a3cac6
kshneo0 <kshneo@gmail.com>
2022-04-14 06:56:17 +0900

    44. Set up the MovieInfo Document

---
+++ movies-info-service/src/main/java/com/reactivespring/domain/MovieInfo.java
@@ -0,0 +1,26 @@
+package com.reactivespring.domain;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Document
+public class MovieInfo {
+	
+	@Id
+	private String MovieInfoId;
+	private String name;
+	private Integer year;
+	private List<String> cast;
+	private LocalDate release_date;
+	
+}
