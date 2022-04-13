3733e587dd118eea6caf45902b346c6b22187e51
kshneo0 <kshneo@gmail.com>
2022-04-13 21:59:26 +0900

    37. Build a simple Non Blocking API - Mono

--- movies-info-service/src/main/java/com/reactivespring/controller/FluxMonoController.java
+++ movies-info-service/src/main/java/com/reactivespring/controller/FluxMonoController.java
@@ -4,6 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @RestController
 public class FluxMonoController {
@@ -11,5 +12,10 @@ public class FluxMonoController {
 	@GetMapping("/flux")
 	public Flux<Integer> flux(){
 		return Flux.just(1,2,3).log();
+	}
+	
+	@GetMapping("/mono")
+	public Mono<String> helloWorldMono(){
+		return Mono.just("hello-world").log();
 	}
 }
