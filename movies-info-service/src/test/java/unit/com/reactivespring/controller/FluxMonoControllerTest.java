09e7e933a6bedfc6b1515928219353bd4f331641
kshneo0 <kshneo@gmail.com>
2022-04-14 06:05:11 +0900

    41. Different Approaches of Testing an Endpoint

--- movies-info-service/src/test/java/unit/com/reactivespring/controller/FluxMonoControllerTest.java
+++ movies-info-service/src/test/java/unit/com/reactivespring/controller/FluxMonoControllerTest.java
@@ -1,12 +1,12 @@
 package com.reactivespring.controller;
 
-import static org.junit.jupiter.api.Assertions.fail;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
 import org.springframework.test.web.reactive.server.WebTestClient;
+
+import reactor.test.StepVerifier;
 
 @WebFluxTest(controllers = FluxMonoController.class)
 @AutoConfigureWebTestClient
@@ -26,6 +26,36 @@ class FluxMonoControllerTest {
 			.is2xxSuccessful()
 			.expectBodyList(Integer.class)
 			.hasSize(3);
+	}
+	
+	@Test
+	void flux_approach2() {
+		var flux = webTestClient
+					.get()
+					.uri("/flux")
+					.exchange()
+					.expectStatus()
+					.is2xxSuccessful()
+					.returnResult(Integer.class)
+					.getResponseBody();
+		
+		StepVerifier.create(flux)
+			.expectNext(1,2,3)
+			.verifyComplete();
+	}
+	
+	@Test
+	void flux_approach3() {
+		webTestClient
+				.get()
+				.uri("/flux")
+				.exchange()
+				.expectStatus()
+				.is2xxSuccessful()
+				.expectBodyList(Integer.class)
+				.consumeWith(listEntityExchangeResult -> {
+					listEntityExchangeResult.getResponseBody();
+				});
 	}
 
 }
