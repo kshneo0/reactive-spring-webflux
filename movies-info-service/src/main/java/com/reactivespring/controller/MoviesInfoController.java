b7eceebf58c252187beee6a44938e7410194b255
kshneo0 <kshneo@gmail.com>
2022-04-14 09:52:50 +0900

    53. Build a POST endpoint to create a new MovieInfo

---
+++ movies-info-service/src/main/java/com/reactivespring/controller/MoviesInfoController.java
@@ -0,0 +1,31 @@
+package com.reactivespring.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.reactivespring.cservice.MovieInfoService;
+import com.reactivespring.domain.MovieInfo;
+
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping("v1")
+public class MoviesInfoController {
+	
+	private MovieInfoService movieInfoService;
+
+	public MoviesInfoController(MovieInfoService movieInfoService) {
+		this.movieInfoService = movieInfoService;
+	}
+
+	@PostMapping("/movieinfos")
+	@ResponseStatus(HttpStatus.CREATED)
+	public Mono<MovieInfo> addMovieInfo(@RequestBody MovieInfo movieInfo){
+		return movieInfoService.addMovieInfo(movieInfo).log();
+	}
+
+}
