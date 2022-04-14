ed4726fccf3dfe8e70d84e067b74a987a1662a80
kshneo0 <kshneo@gmail.com>
2022-04-15 05:30:17 +0900

    75. Build the POST endpoint for creating a new Review

--- movies-review-service/src/main/java/com/reactivespring/handler/ReviewHandler.java
+++ movies-review-service/src/main/java/com/reactivespring/handler/ReviewHandler.java
@@ -1,5 +1,26 @@
 package com.reactivespring.handler;
 
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+import com.reactivespring.domain.Review;
+import com.reactivespring.repository.ReviewReactiveRepository;
+
+import reactor.core.publisher.Mono;
+
+@Component
 public class ReviewHandler {
+	
+	private ReviewReactiveRepository reviewReactiveRepository;
+
+	public ReviewHandler(ReviewReactiveRepository reviewReactiveRepository) {
+		this.reviewReactiveRepository = reviewReactiveRepository;
+	}
+
+	public Mono<ServerResponse> addReview(ServerRequest request) {
+		return request.bodyToMono(Review.class)
+			.flatMap(reviewReactiveRepository::save);
+	}
 
 }
--- movies-review-service/src/main/java/com/reactivespring/router/ReviewRouter.java
+++ movies-review-service/src/main/java/com/reactivespring/router/ReviewRouter.java
@@ -1,19 +1,22 @@
 package com.reactivespring.router;
 
-import static org.springframework.web.reactive.function.server.RouterFunctions.route;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.server.RouterFunction;
-import org.springframework.web.reactive.function.server.ServerResponse; 
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+import static org.springframework.web.reactive.function.server.RouterFunctions.route;
+
+import com.reactivespring.handler.ReviewHandler; 
 
 @Configuration
 public class ReviewRouter {
 	
 	@Bean
-	public RouterFunction<ServerResponse> reviewRoutes(){
+	public RouterFunction<ServerResponse> reviewRoutes(ReviewHandler reviewHandler){
 		return route()
 				.GET("/v1/helloworld",(request -> ServerResponse.ok().bodyValue("HelloWorld")))
+				.POST("/v1/reviews",request -> reviewHandler.addReview(request))
 				.build();
 	}
 }
