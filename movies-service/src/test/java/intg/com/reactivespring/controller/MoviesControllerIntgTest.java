9f52201a559401f6f1494cb7188ce1edf5651f24
kshneo0 <kshneo@gmail.com>
2022-04-16 17:13:57 +0900

    102. Stubs for MoviesInfoService and ReviewService

--- movies-service/src/test/java/intg/com/reactivespring/controller/MoviesControllerIntgTest.java
+++ movies-service/src/test/java/intg/com/reactivespring/controller/MoviesControllerIntgTest.java
@@ -1,16 +1,24 @@
 package com.reactivespring.controller;
 
-import org.springframework.boot.test.autoconfigure.web.client.AutoConfigureWebClient;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.TestPropertySource;
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static com.github.tomakehurst.wiremock.client.WireMock.get;
+import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlPathEqualTo;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.Objects;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.client.AutoConfigureWebClient;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import com.reactivespring.domain.Movie;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("test")
@@ -21,4 +29,35 @@ import org.springframework.test.context.TestPropertySource;
         "restClient.reviewsUrl=http://localhost:8084/v1/reviews",
 })
 public class MoviesControllerIntgTest {
+	
+	@Autowired
+	WebTestClient webTestClient;
+	
+	@Test
+	void retrieveMovieById() {
+		
+		var movieId = "abc";
+		stubFor(get(urlEqualTo("/v1/movieinfos"+ "/" + movieId))
+				.willReturn(aResponse()
+				.withHeader("Content-Type", "applicaytion/json")
+				.withBodyFile("movieinfo.json")));
+		
+        stubFor(get(urlPathEqualTo("/v1/reviews"))
+                .willReturn(aResponse()
+                        .withHeader("Content-Type", "application/json")
+                        .withBodyFile("reviews.json")));
+		
+		webTestClient
+			.get()
+			.uri("/v1/movie/{id}", movieId)
+			.exchange()
+			.expectStatus().isOk()
+			.expectBody(Movie.class)
+			.consumeWith(movieEntityExchangeResult -> {
+				var movie = movieEntityExchangeResult.getResponseBody();
+				assert Objects.requireNonNull(movie).getReviewList().size()==2;
+				assertEquals("Batman Begins", movie.getMovieInfo().getName());				
+			});
+		
+	}
 }
