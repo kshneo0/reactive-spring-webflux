e60219caf1f792a00b671f9d214a2c5cbe556b14
kshneo0 <kshneo@gmail.com>
2022-04-16 10:25:37 +0900

    99. Implement the 4XX and 5XX error handling in ReviewsClient

--- movies-service/src/main/java/com/reactivespring/client/ReviewsRestClient.java
+++ movies-service/src/main/java/com/reactivespring/client/ReviewsRestClient.java
@@ -1,14 +1,19 @@
 package com.reactivespring.client;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import com.reactivespring.domain.Review;
 
+import com.reactivespring.exception.ReviewsClientException;
+import com.reactivespring.exception.ReviewsServerException;
+
 import lombok.extern.slf4j.Slf4j;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @Component
 @Slf4j
@@ -34,6 +39,23 @@ public class ReviewsRestClient {
 			.get()
 			.uri(url)
 			.retrieve()
+			.onStatus(HttpStatus::is4xxClientError, clientResponse -> {
+				log.info("Status code id : {}", clientResponse.statusCode().value() );
+				if(clientResponse.statusCode().equals(HttpStatus.NOT_FOUND)) {
+					return Mono.empty();
+				}				
+				return clientResponse.bodyToMono(String.class)
+						.flatMap(responseMessage -> Mono.error(new ReviewsClientException(
+								responseMessage)));
+			})
+			.onStatus(HttpStatus::is5xxServerError, clientResponse -> {
+				log.info("Status code id : {}", clientResponse.statusCode().value() );
+				
+				return clientResponse.bodyToMono(String.class)
+						.flatMap(responseMessage -> Mono.error(new ReviewsServerException(
+								"Server Exception in ReviewsService " + responseMessage)));
+								
+			})
 			.bodyToFlux(Review.class);
 		
 	}
