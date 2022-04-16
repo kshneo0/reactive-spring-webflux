0c8447b8fa21edbf7aaf1c7b411a2dc9417163e3
kshneo0 <kshneo@gmail.com>
2022-04-16 20:05:58 +0900

    103. Simulate 4xx Errors in Wiremock

--- movies-service/src/test/java/intg/com/reactivespring/controller/MoviesControllerIntgTest.java
+++ movies-service/src/test/java/intg/com/reactivespring/controller/MoviesControllerIntgTest.java
@@ -39,25 +39,81 @@ public class MoviesControllerIntgTest {
 		var movieId = "abc";
 		stubFor(get(urlEqualTo("/v1/movieinfos"+ "/" + movieId))
 				.willReturn(aResponse()
-				.withHeader("Content-Type", "applicaytion/json")
+				.withHeader("Content-Type", "application/json")
 				.withBodyFile("movieinfo.json")));
 		
         stubFor(get(urlPathEqualTo("/v1/reviews"))
                 .willReturn(aResponse()
                         .withHeader("Content-Type", "application/json")
                         .withBodyFile("reviews.json")));
+  
 		
-		webTestClient
-			.get()
-			.uri("/v1/movie/{id}", movieId)
-			.exchange()
-			.expectStatus().isOk()
-			.expectBody(Movie.class)
-			.consumeWith(movieEntityExchangeResult -> {
-				var movie = movieEntityExchangeResult.getResponseBody();
-				assert Objects.requireNonNull(movie).getReviewList().size()==2;
-				assertEquals("Batman Begins", movie.getMovieInfo().getName());				
-			});
+        webTestClient.get()
+        .uri("/v1/movies/{id}", "abc")
+        .exchange()
+        .expectStatus().isOk()
+        .expectBody(Movie.class)
+        .consumeWith(movieEntityExchangeResult -> {
+                    var movie = movieEntityExchangeResult.getResponseBody();
+                    assert Objects.requireNonNull(movie).getReviewList().size() == 2;
+                    assertEquals("Batman Begins", movie.getMovieInfo().getName());
+                }
+        );
+		
+	}
+	
+	@Test
+	void retrieveMovieById_404() {
+		
+		var movieId = "abc";
+		stubFor(get(urlEqualTo("/v1/movieinfos"+ "/" + movieId))
+				.willReturn(aResponse()
+				.withStatus(404)
+						));
+		
+		stubFor(get(urlPathEqualTo("/v1/reviews"))
+				.willReturn(aResponse()
+						.withHeader("Content-Type", "application/json")
+						.withBodyFile("reviews.json")));
+		
+		
+		webTestClient.get()
+		.uri("/v1/movies/{id}", "abc")
+		.exchange()
+		.expectStatus()
+		.is4xxClientError()
+		.expectBody(String.class)
+		.isEqualTo("There is no MovieInfo Available for the passed in Id : abc");
+		
+	}
+	
+	@Test
+	void retrieveMovieById_reviews_404() {
+		
+		var movieId = "abc";
+		stubFor(get(urlEqualTo("/v1/movieinfos"+ "/" + movieId))
+				.willReturn(aResponse()
+						.withHeader("Content-Type", "application/json")
+						.withBodyFile("movieinfo.json")
+						));
+		
+		stubFor(get(urlPathEqualTo("/v1/reviews"))
+				.willReturn(aResponse()
+						.withStatus(404)
+						));
+		
+		
+		webTestClient.get()
+		.uri("/v1/movies/{id}", "abc")
+		.exchange()
+		.expectStatus().isOk()
+        .expectBody(Movie.class)
+        .consumeWith(movieEntityExchangeResult -> {
+                    var movie = movieEntityExchangeResult.getResponseBody();
+                    assert Objects.requireNonNull(movie).getReviewList().size() == 0;
+                    assertEquals("Batman Begins", movie.getMovieInfo().getName());
+                }
+        );
 		
 	}
 }
