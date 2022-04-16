7eb5c8a74bec1b9234783aff4cee98b16d60b67f
kshneo0 <kshneo@gmail.com>
2022-04-16 10:36:03 +0900

    101. SetUp Wiremock in Integration Tests

--- movies-service/src/test/java/intg/com/reactivespring/controller/MoviesControllerIntgTest.java
+++ movies-service/src/test/java/intg/com/reactivespring/controller/MoviesControllerIntgTest.java
@@ -1,4 +1,24 @@
 package com.reactivespring.controller;
 
+import org.springframework.boot.test.autoconfigure.web.client.AutoConfigureWebClient;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
+
+import org.springframework.boot.test.autoconfigure.web.client.AutoConfigureWebClient;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
+@AutoConfigureWebClient
+@AutoConfigureWireMock(port = 8084) // automaticaly spins up a httpserver in port 8084
+@TestPropertySource(properties = {
+        "restClient.moviesInfoUrl=http://localhost:8084/v1/movieinfos",
+        "restClient.reviewsUrl=http://localhost:8084/v1/reviews",
+})
 public class MoviesControllerIntgTest {
 }
